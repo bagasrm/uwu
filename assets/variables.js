54d99910b96e1e15db83a895a7108f88bff22bbe
bagasrm <bagasrm99@gmail.com>
2020-07-05 10:43:01 +0700

    Add files via upload

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["My baby", "Babe", "Sayang", "My be"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
